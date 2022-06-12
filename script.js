a9c9d4d9787c0d6a45c6bfaa65ad291055f0f342
Christine Jackson <mom2MB464@gmail.com>
2022-06-12 15:17:12 -0500

    Submitting Project 10 CodeAlong

---
+++ script.js
@@ -0,0 +1,134 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bkmrk-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bkmrk-container');
+
+let bookmarks = [];
+
+// SHOW MODAL, FOCUS ON INPUT
+function showModal() {
+    modal.classList.add('show-modal');
+    websiteNameEl.focus();
+}
+
+// Modal Event Listener
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// VALIDATE FORM
+function validate(nameValue, urlValue) {
+    const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+      alert('Please submit values for both fields.');
+      return false;
+    }
+    if (!urlValue.match(regex)) {
+      alert('Please provide a valid web address.');
+      return false;
+    }
+    // VALID
+    return true;
+  }
+
+  // BUILD BOOKMARKS
+  function buildBookmarks() {
+    // REMOVE ALL BOOKMARK ELEMENTS
+    bookmarksContainer.textContent = '';
+    // BUILD ITEMS
+    bookmarks.forEach((bookmark) => {
+        const { name, url } = bookmark;
+        // ITEM
+        const item = document.createElement('div');
+        item.classList.add('item');
+        // CLOSE ICON
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('fa-solid', 'fa-xmark');
+        closeIcon.setAttribute('title', 'Delete Bookmark');
+        closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+        // FAVICON / LINK CONTAINER
+        //const linkInfo = document.add('name');
+        const linkInfo = document.createElement('div');
+        linkInfo.classList.add('name');
+        // FAVICON
+        const favicon = document.createElement('img');
+        favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
+        favicon.setAttribute('alt', 'Favicon');
+        // LINK
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}`);
+        link.setAttribute('target', '_blank');
+        link.textContent = name;
+        // APPEND TO BOOKMARKS CONTAINER
+        linkInfo.append(favicon, link);
+        item.append(closeIcon, linkInfo)
+        bookmarksContainer.appendChild(item);
+    });
+  }
+
+  // FETCH BOOKMARKS
+  function fetchBookmarks() {
+    // GET BOOKMARKS FROM localStorage IF AVAILABLE
+    if (localStorage.getItem('bookmarks')) {
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else {
+        // CREATE BOOKMARKS ARRAY IN localStorage
+        bookmarks = [
+            {
+                name: 'cjWebDev Portfolio',
+                url: 'https://christinejackson464.github.io/'
+            },
+        ];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    buildBookmarks();
+  }
+
+// DELETE BOOKMARK
+function deleteBookmark(url) {
+    bookmarks.forEach((bookmark, i) => {
+        if (bookmark.url === url) {
+            bookmarks.splice(i, 1);
+        }
+    });
+    // UPDATE BOOKMARKS ARRAY IN localStorage, REPOPULATE DOM
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+}
+
+// HANDLE DATA FROM FORM
+function storeBookmark (e) {
+    e.preventDefault();
+   const nameValue = websiteNameEl.value;
+   let urlValue = websiteUrlEl.value;
+   // Add 'https://' if not there
+   if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+    urlValue = `https://${urlValue}`;
+   }
+     // Validate
+    if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+    // SET BOOKMARK OBJ, ADD TO ARRAY
+   const bookmark = {
+    name: nameValue,
+    url: urlValue,
+   };
+   bookmarks.push(bookmark);
+   // SET BOOKMARKS IN localStorage, FETCH, RESET INPUT FIELDS
+   localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+   fetchBookmarks();
+   bookmarkForm.reset();
+   websiteNameEl.focus();
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
+
+// ON LOAD, FETCH BOOKMARKS
+fetchBookmarks();
+
